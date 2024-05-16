ffa2aae88d80ca1f234fcf9dbdf659c574dd8c7d
David Mondok <3883758+davidmondok@users.noreply.github.com>
2024-05-16 13:38:31 +0200

    make config universally usable

--- inc/functions.php
+++ inc/functions.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 if (!function_exists('app')) {
     function app(string $name = ''): mixed
     {
-        $container = \Kaiseki\LaravelHelperMocks\Helper::getContainer();
+        $container = Kaiseki\LaravelHelperMocks\Helper::getContainer();
         if ($container === null) {
             return null;
         }
         if ($name === '') {
             return $container;
         }
+
         return $container->get($name);
     }
 }
@@ -26,22 +27,12 @@ if (!function_exists('resolve')) {
 if (!function_exists('config')) {
     function config(string $path = ''): mixed
     {
-        $container = \Kaiseki\LaravelHelperMocks\Helper::getContainer();
+        $container = Kaiseki\LaravelHelperMocks\Helper::getContainer();
         if ($container === null) {
             return null;
         }
-        $config = \Kaiseki\Config\Config::fromContainer($container);
-        $current = $config->array('laravel');
-        $paths = explode('.', $path);
-        foreach ($paths as $index) {
-            if (
-                !is_array($current)
-                || !array_key_exists($index, $current)
-            ) {
-                return null;
-            }
-            $current = $current[$index];
-        }
-        return $current;
+        $config = Kaiseki\Config\Config::fromContainer($container);
+
+        return $config->softGet($path);
     }
 }
